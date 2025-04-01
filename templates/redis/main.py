a48a2aab19176e30a4161b86c5e1d2806c38a429
maliagap <maliagap@emeal.nttdata.com>
2025-04-01 23:21:54 +0000

    adding template

---
+++ templates/redis/main.py
@@ -0,0 +1,48 @@
+import os
+
+rsgr_name = os.environ['rsgr_name']
+location = os.environ['location']
+infr_env = {
+    'scope': os.environ['scope'],
+    'env': os.environ['env']
+}
+audit = {
+    'codproy': os.environ['codproy'],
+    'owner': os.environ['owner'],
+    'criticidad': os.environ['criticidad'],
+}
+seq_number = os.environ['seq_number']
+
+tags = {
+    'CodigoProyecto'   : audit['codproy'],
+    'NombreOwner'      : audit['owner'],
+    'Criticidad'       : audit['criticidad'],
+    'Ambiente'         : infr_env['env'],
+    'NombreAplicacion' : infr_env['scope'],
+}
+
+redis_skuname = "Premium" if infr_env['env'].lower() == 'prod' else "Basic"
+family = "P" if redis_skuname.lower() == 'premium' else 'C'
+public_network_access_enabled = True if infr_env['env'].lower() == "dev" else False
+resource_redis_code = "redis"
+map_custom_name = {
+	'redis_name' : resource_redis_code + "-" + infr_env['scope'] + "-" + infr_env['env'] + "-" + seq_number
+}
+# template
+template = """resource "azurerm_redis_cache" "redis-%s" {
+	name                = "%s"
+	location            = "%s"
+	resource_group_name = "%s"
+	capacity            = 1
+	family              = "%s"
+	sku_name            = "%s"
+	minimum_tls_version = "1.3"
+	redis_version       = "4"
+	public_network_access_enabled = "%s"
+	redis_configuration {
+	}
+	tags = "%s"
+}"""
+
+print(template %(seq_number, map_custom_name['redis_name'], location, rsgr_name, family, redis_skuname, public_network_access_enabled, tags))
+
---
+++ templates/redis/main.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsgr_name="rsgr" location="location" scope="dev" env="dev" codproy="001"  owner="yo" criticidad="alta" seq_number="0001" python3 $PWD/templates/redis/main.py
