7e3567948eb9fa9ff5876c73301e4a33e488cd9f
maliagap <maliagap@emeal.nttdata.com>
2025-04-02 19:53:10 +0000

    removing hardcoded vars

--- templates/redis/main.sh
+++ templates/redis/main.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rsgr_name="rsgr" location="location" scope="dev" env="dev" codproy="001"  owner="yo" criticidad="alta" seq_number="0001" python3 $PWD/templates/redis/main.py
+#rsgr_name="rsgr" location="location" scope="dev" env="dev" codproy="001"  owner="yo" criticidad="alta" seq_number="0001" 
+python3 $PWD/templates/redis/main.py
