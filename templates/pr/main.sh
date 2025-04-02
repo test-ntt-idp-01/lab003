41292b22407b9b410984616c2dcbab568913c634
maliagap <maliagap@emeal.nttdata.com>
2025-04-02 15:41:16 +0000

    updating pr merge cmd

--- templates/pr/main.sh
+++ templates/pr/main.sh
@@ -7,4 +7,4 @@ PR_ID=$(echo "$PR_LINK" | tail -n1 | cut -d "/" -f7)
 echo $PR_ID
 # approving pr
 # gh pr review --approve -R test-ntt-idp-01/lab003-dev  $PR_ID
-gh pr merge -b "merged by github actions" --admin --auto -R test-ntt-idp-01/lab003-dev  $PR_ID 
+gh pr merge -b "merged by github actions"  --auto -R test-ntt-idp-01/lab003-dev  $PR_ID 
