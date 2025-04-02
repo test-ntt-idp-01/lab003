17520361e10097993ec4818bd9915563904428dc
maliagap <maliagap@emeal.nttdata.com>
2025-04-02 15:29:45 +0000

    update

--- templates/pr/main.sh
+++ templates/pr/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # creating pr and getting url
-PR_LINK=$(gh pr create -B main -b "adding new redis component to iac" -t "deployed with github actions" -R test-ntt-idp-01/lab003-dev -H custom-new-branch)
+PR_LINK=$(gh pr create -a "@me" -B main -b "adding new redis component to iac" -t "deployed with github actions" -R test-ntt-idp-01/lab003-dev -H custom-new-branch)
 echo $PR_LINK
 # getting pr id
 PR_ID=$(echo "$PR_LINK" | tail -n1 | cut -d "/" -f7)
